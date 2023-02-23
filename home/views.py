43d8230da56d78e4de8f6e5b5a913c4a21394ff5
glind <greg@buildly.io>
2023-02-23 08:43:17 -0800

    update developer page

--- home/views.py
+++ home/views.py
@@ -4,6 +4,9 @@ from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from .forms import ContactMailForm
+import requests
+from django.urls import reverse
+from django.conf import settings
 
 def contactView(request):
     if request.method == "GET":
@@ -25,3 +28,27 @@ def contactView(request):
 
 def successView(request):
     return render(request, "home/success.html")
+
+
+def register(request):
+    if request.method == 'POST':
+        organization_name = request.POST.get('organization_name')
+        technical_founder = request.POST.get('technical_founder')
+        agency = request.POST.get('agency')
+
+        # Submit data to API endpoint
+        api_endpoint = settings.API_URL + '/register/'
+        data = {
+            'organization_name': organization_name,
+            'technical_founder': technical_founder,
+            'agency': agency
+        }
+        response = requests.post(api_endpoint, data=data)
+
+        if response.status_code == 200:
+            return render(request, 'registration_success.html')
+        else:
+            error_message = 'Failed to register. Error code: {}'.format(response.status_code)
+            return render(request, 'register.html', {'error_message': error_message})
+    else:
+        return render(request, 'register.html')
